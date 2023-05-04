0b8b91e235110011b50b6ec52806b55632d7f3e8
Koyo Nakayama <taiyou.003.084@gmail.com>
2023-05-04 15:20:47 +0900

    modified Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,16 @@
 import { useState } from 'react';
 
 function Form() {
-    const [form, setForm] = useState({ name: "", email: "" });
+    const [form, setForm] = useState({
+        pregnancies: "",
+        glucose: "",
+        blood_presure: "",
+        skin_thickness: "",
+        inslulin_level: "",
+        bmi: "",
+        diabetes_pedigree: "",
+        age: ""
+    });
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -10,19 +19,24 @@ function Form() {
     };
 
     const onChange = (event) => {
-        console.log("Changed input field");
         const name = event.target.name;
         const value = event.target.value;
-
-        console.log(`event.target.name: ${name}, event.target.value: ${value}`)
 
         setForm({ ...form, [name]: value });
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" name="name" onChange={onChange} placeholder="Name (e.g. John)" />
-            <input type="email" name="email" onChange={onChange} placeholder="Email (e.g. john.doe@example.com)" />
+            <h4>Diabetes Prediction Model</h4>
+            <p>Example to Predir Probability of Diabetes</p>
+            <input type="number" name="pregnancies" onChange={onChange} placeholder="Number of pregnancies" />
+            <input type="number" name="glucose" onChange={onChange} placeholder="Glucose level in Sugar" />
+            <input type="number" name="blood_presure" onChange={onChange} placeholder="Blood Presure" />
+            <input type="number" name="skin_thickness" onChange={onChange} placeholder="Skin Thickness" />
+            <input type="number" name="inslulin_level" onChange={onChange} placeholder="Insulin Level" />
+            <input type="number" name="bmi" onChange={onChange} placeholder="Body Mass Index (BMI)" />
+            <input type="number" name="diabetes_pedigree" onChange={onChange} placeholder="Diabetes pedigree Function" />
+            <input type="number" name="age" onChange={onChange} placeholder="Age" />
             <button type="submit">Submit Form</button>
         </form>
     );
