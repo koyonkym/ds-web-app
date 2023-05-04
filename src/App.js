e045b082c3a399b4ffe43dbc5d578a4cd7866efc
Koyo Nakayama <taiyou.003.084@gmail.com>
2023-05-04 15:08:11 +0900

    modified App.jp and deleted User.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-// import { User, SecondUser } from './User'
 import From from './Form'
-import Data from './Data'
 
 function App() {
-  const name = "Emily";
-  const age = 40;
-
-  const clickMe = () => {
-    console.log("I got clicked!");
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Data />
         <From />
-        {/* <User name={name} age={age} function={clickMe}/>
-        <SecondUser /> */}
       </header>
     </div>
   );
